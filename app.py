d3a9b4d263903f4f275eb0571ee2d2cb1c229c53
Shreyash Verma <superbshreyash@gmail.com>
2026-08-22 10:20:01 +0530

    Initial commit: Basic Flask app with Waitress

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # This will help us prove which environment is running later
+    env_name = os.environ.get('ENV_NAME', 'Local Development')
+    return f"Hello! This is running in the {env_name} environment."
+
+if __name__ == '__main__':
+    from waitress import serve
+    # We use 8080 as a default test port
+    print("Server starting on port 8080...")
+    serve(app, host='0.0.0.0', port=8080)
